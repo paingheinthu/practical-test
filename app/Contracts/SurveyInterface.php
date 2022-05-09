7173806eaab9e6a90a3d6d20b30c8a3144d21fce
Paing Hein Thu <painghein.thu@frontiir.net>
2022-05-09 16:38:49 +0630

    add the survey service

--- app/Contracts/SurveyInterface.php
+++ app/Contracts/SurveyInterface.php
@@ -2,6 +2,13 @@
 
 namespace App\Contracts;
 
+use App\Models\Survey;
+
 interface SurveyInterface
 {
+    public function createSurvey(string $title, string $description = ""): ?Survey;
+
+    public function getSurvey(string|int $filter): ?Survey;
+
+    public function disableSurvey(Survey $survey): bool;
 }
---
+++ app/Services/SurveyService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Survey;
+use App\Contracts\SurveyInterface;
+
+class SurveyService implements SurveyInterface
+{
+    public function __construct(protected Survey $survey)
+    {
+    }
+
+    public function createSurvey(string $title, string $description = ""): ?Survey
+    {
+        return $this->survey->create(
+            [
+                'title' => $title,
+                'description' => $description
+            ]
+        );
+    }
+
+    public function getSurvey(string|int $filter): ?Survey
+    {
+        return $this->survey
+            ->where(is_int($filter) ? 'id' : 'title', $filter)
+            ->first();
+    }
+
+    public function disableSurvey(Survey $survey): bool
+    {
+        return $survey->update([
+            'status' => 0,
+        ]);
+    }
+}
