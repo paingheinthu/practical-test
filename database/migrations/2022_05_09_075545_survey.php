649126c4aafb655bf1c551b983d30325101d6721
Paing Hein Thu <painghein.thu@frontiir.net>
2022-05-09 16:15:58 +0630

    add the survey, question and answer tables migration

---
+++ database/migrations/2022_05_09_075545_survey.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('mysql')->create('survey', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+            $table->index('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey');
+    }
+};
---
+++ database/migrations/2022_05_09_094235_answer.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('mysql')->create('answers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('survey_id');
+            $table->bigInteger('user_id');
+            $table->bigInteger('question_id');
+            $table->string('answer');
+            $table->timestamps();
+            $table->unique(['survey_id', 'question_id', 'user_id'], 'survey_user_question_uniq');
+            $table->index('survey_id');
+            $table->index('question_id');
+            $table->index('user_id');
+            $table->index(['survey_id', 'question_id']);
+            $table->index(['survey_id', 'user_id']);
+            $table->index(['user_id', 'question_id']);
+            $table->index(['user_id', 'survey_id', 'question_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+};
